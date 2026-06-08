8e3ea2be0121a96d6040a9fa5eb2a5cb8cfa2bd7
Vidya Meenakshi <drmrprist@gmail.com>
2026-06-09 00:40:39 +0800

    proof-point-2: SQL Server to Azure SQL DB migration complete - schema + data migrated, row counts verified

---
+++ migration/sqlserver/data_migration.sql
@@ -0,0 +1,13 @@
+                                                                                                                                                                                      
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO CatalogBrands (Id, Brand) VALUES (1, 'Azure');                                                                                                                            
+INSERT INTO CatalogBrands (Id, Brand) VALUES (2, '.NET');                                                                                                                             
+INSERT INTO CatalogBrands (Id, Brand) VALUES (3, 'Visual Studio');                                                                                                                    
+INSERT INTO CatalogBrands (Id, Brand) VALUES (4, 'SQL Server');                                                                                                                       
+INSERT INTO CatalogBrands (Id, Brand) VALUES (5, 'Other');                                                                                                                            
+                                                                                                                                                                                    
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO CatalogTypes (Id, Type) VALUES (1, 'Mug');                                                                                                                              
+INSERT INTO CatalogTypes (Id, Type) VALUES (2, 'T-Shirt');                                                                                                                          
+INSERT INTO CatalogTypes (Id, Type) VALUES (3, 'Sheet');                                                                                                                            
+INSERT INTO CatalogTypes (Id, Type) VALUES (4, 'USB Memory Stick');                                                                                                                 
